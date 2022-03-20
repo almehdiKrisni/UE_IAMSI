753b8545c299ced84b4791cae09f648807e6bdf0
unknown <almehdi.krisni@gmail.com>
2022-03-21 00:03:41 +0100

    First part of TME4 done

--- TME4/RapportTME4.md
+++ TME4/RapportTME4.md
@@ -2,21 +2,42 @@
 
 Membres du binôme :
 - KRISNI Almehdi (3800519)
-- ARICHANDRA Santhos
+- ARICHANDRA Santhos (3802651)
 
-## Exercice 1
+## **Exercice 1**
 
-Pour la première théorie, on obtient le résultat suivant avec Glucose :
+Pour la première théorie, on obtient le résultat suivant avec Glucose :<br/>
 s SATISFIABLE
 v 1 -2 -3 4 -5 -6 7 -8 0
 
-Pour la deuxième théorie, on obtient le résultat suivant :
+Pour la deuxième théorie, on obtient le résultat suivant :<br/>
 s SATISFIABLE
 v 1 -2 -3 4 5 -6 -7 -8 -9 10 -11 12 -13 -14 -15 -16 0
 
-On vérifie que la formule F de l'exercice 2 est bien insatisfiable. On obtient le résultat suivant :
+On vérifie que la formule F de l'exercice 2 est bien insatisfiable. On obtient le résultat suivant :<br/>
 s UNSATISFIABLE
 
-## Exercice 2
+## **Exercice 2**
 
 Afin de coder les différentes variables et contraintes du sujet, on décide de mettre en place un ficher allant réaliser des opérations d'écriture de fichier lors de son exécution.
+
+Dans le fichier champ.py, on retrouve les méthodes codage et decodage permettant de transformer les variables obtenues.
+
+### **Question 1**
+Les jours étant compris dans l'ensemble [0, nj - 1], les équipes dans l'ensemble [0, ne - 1], il existe donc (nj * ne * (ne - 1)) combinaisons, donc de variables propositionnelles.
+
+On (ne * (ne - 1)) puisqu'une équipe ne peut pas jouer contre elle-même.
+
+### **Question 2 et 3**
+Il s'agit des méthodes codage et decodage du fichier *champ.py*.
+
+## **Exercice 3**
+
+### **Question 1**
+Les méthodes *auMoinsUn* et *auPlusUn* sont implémentées dans le fichier code.
+
+### **Question 3**
+On met en place la méthode *generationCNF* allant écrire tout le système dans le fichier *champSys.cnf*.
+
+On écrit un premier système avec 3 équipes et 4 jours. On obtient le système suivant :
+
---
+++ TME4/champ.py
@@ -0,0 +1,101 @@
+# Fichier python concernant la mise en place des fichiers variables-contraintes de Championnat
+import pycosat
+
+# Partie 2 - Modélisation
+
+# Question 2
+def codage(ne, nj, j, x, y) :
+    # Méthode de codage de la variable k en fonction de ne, nj, j, x et y
+    return j * ne ** 2 + x * ne + y + 1
+
+# Question 3
+def decodage(k, ne) :
+    # Méthode de décodage des variables j, x et y en fonction de k et ne
+    # On utilise les multiplications divisions entières afin de retrouver les valeurs
+    j = (k - 1) // ne ** 2
+    x = ((k - 1) % ne ** 2) // ne
+    y = (k - 1) % ne
+    return j, x, y
+
+## Partie 3 - Génération d'un planning de matchs
+
+# Question 1 - Contraintes de cardinalité
+def auMoinsUn(v) :
+    # Création d'une clause au format DIMACS traduisant la contrainte "au moins un" sur les variables
+    return " " .join(str(i) for i in v) + " 0\n"
+
+def auPlusUn(v) :
+    # Création des clauses au format DIMACS traduisant la contrainte "au plus un" sur les variables
+    # On les compose en utilisant la méthode auMoinsUn
+    c = ""
+
+    # Création de tous les couples de variables possibles
+    for i in range(len(v)):
+        for j in range(i + 1, len(v)):
+            c += auMoinsUn([-v[i], -v[j]])
+            
+    return c
+
+# Question 2 - Traduction du problème
+def encoderC1(ne, nj) :
+    # La contrainte 1 stipule qu'une équipe ne peut jouer qu'un seul match par jour
+    c = ""
+
+    # On boucle sur les équipes x et les jours j
+    for x in range(ne) :
+        for j in range(nj) :
+
+            # Liste des variables apparaissant dans la contrainte pour (x,j)
+            v = []
+            for y in range(ne) :
+                if (x != y) :
+                    v.append(codage(ne, nj, j, x, y))
+                    v.append(codage(ne, nj, j, y, x))
+
+            c += auPlusUn(v)
+            
+    # On retourne l'ensemble des clauses
+    return c
+
+def encoderC2(ne, nj):
+    # La contrainte 2 stipule que chaque équipe doit faire un match avec toutes les autres
+    # équipes (une fois à domicile et une fois à l'extérieur)
+    c = ""
+    
+    # Bouclage sur chaque paire d'équipes adverses (x,y)
+    for x in range(ne) :
+        for y in range(ne) :
+            if (x != y) :
+                # Liste des variables apparaissant dans la contrainte pour (x,y)
+                v = []
+
+                for j in range(nj):
+                    v.append(codage(ne, nj, j, x, y))
+
+                c += auPlusUn(v)
+                c += auMoinsUn(v)
+    
+    # On retourne l'ensemble des clauses
+    return c
+
+def encoder(ne, nj) :
+    # On met en place une méthode allant permettre l'encodage de l'ensemble directement sur
+    # un fichier
+    c = encoderC1(ne, nj) + encoderC2(ne, nj)
+
+    # On compte le nombre de contraintes avec le nombre de retour à la ligne et le nombre de variables
+    nbC = c.count('\n')
+    nbV = nj * ne ** 2
+
+    # On retourne le corps de contraintes
+    return "p cnf " + str(nbV) + " " + str(nbC) +"\n" + c
+
+# Question 3
+def generationCNF(ne, nj) :
+    # Méthode permet d'écrire toutes les contraintes dans le fichier champSys.cnf
+    with open("champSys.cnf", 'w') as file :
+        file.write(encoder(ne, nj))
+
+
+# Partie EXEC
+generationCNF(3, 4)
